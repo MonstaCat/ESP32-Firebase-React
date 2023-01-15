bda8406da530f050655fe01039af20f94e2e8b6a
TempoyakBelacan <andikarizkipradanaa@gmail.com>
2023-01-15 22:15:28 +0700

    Change theme to light

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,20 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React from 'react';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle } from '@ionic/react';
+
 import './Home.css';
 
-const Home: React.FC = () => {
+function Example() {
   return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
+    <IonCard>
+      <IonCardHeader>
+        <IonCardTitle>Card Title</IonCardTitle>
+        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+      </IonCardHeader>
 
-export default Home;
+      <IonCardContent>
+        Here's a small text description for the card content. Nothing more, nothing less.
+      </IonCardContent>
+    </IonCard>
+  );
+}
+export default Example;
