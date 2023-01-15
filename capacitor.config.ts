d4485d52889018c7670497a8b38372dc1e95569b
TempoyakBelacan <andikarizkipradanaa@gmail.com>
2023-01-15 21:26:20 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ESP32-Firebase-React',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
